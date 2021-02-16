236d1778c9ecc1e9202ee4cc15a6fa40c17621c2
Query-Interface <7815125+Query-Interface@users.noreply.github.com>
2021-02-16 18:38:31 +0100

    add response for question 66091085

---
+++ dotNET/AspWithSessionOnMultipleNodes/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using app.Models;
+
+namespace app.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            byte[] value = new byte[0];
+
+            string sVal = HttpContext.Session.GetString("lastVisit");;
+            if (sVal == null) {
+                sVal = "_";
+            }
+
+            HttpContext.Session.SetString("lastVisit", DateTime.UtcNow.ToString());
+            string host = Dns.GetHostName();
+            return Content($"Host: {host};  Last seen: {sVal}");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ dotNET/AspWithSessionOnMultipleNodes/Startup.cs
@@ -0,0 +1,77 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.StackExchangeRedis;
+using StackExchange.Redis;
+
+namespace app
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+			// allows multiple instance of the same application
+            services.AddDataProtection()
+                .SetApplicationName("myapp_session")
+                .PersistKeysToStackExchangeRedis(ConnectionMultiplexer.Connect("redis:6379,password=cE3nNEXHmvGCwdqxxxxxxxxxxx"),
+                    "DataProtection-Keys");
+
+            services.AddControllersWithViews();
+
+			// distributed shared cache
+            services.AddStackExchangeRedisCache(action => {
+                action.InstanceName = "redis";
+                action.Configuration = "redis:6379,password=cE3nNEXHmvGCwdqxxxxxxxxxxx";
+            });
+
+            services.AddSession(options => {
+                options.Cookie.Name = "myapp_session";
+                options.IdleTimeout = TimeSpan.FromMinutes(60 * 24);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseSession();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
